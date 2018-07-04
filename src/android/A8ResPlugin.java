e8f07a55b45b14e5852beb2d0d272f9d632b57ed
TTEBD Dick <dick.he@tech-trans.com>
2018-07-04 15:15:22 +0800

    增加调用A8打印机功能

--- src/android/A8ResPlugin.java
+++ src/android/A8ResPlugin.java
@@ -1,5 +1,9 @@
 package com.ttebd.a8ResPlugin;
 
+import android.util.Log;
+
+import com.ttebd.a8ResPlugin.PrinterMain;
+
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 
@@ -9,24 +13,65 @@ import org.json.JSONObject;
 
 /**
  * This class echoes a string called from JavaScript.
+ *
+ *
  */
+
+
+//sourceSets {
+//        main {
+//        jniLibs.srcDirs = ['libs']
+//        }}
 public class A8ResPlugin extends CordovaPlugin {
+    public static final String TAG = "A8ResPlugin";
+
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("coolMethod")) {
-            String message = args.getString(0);
-            this.coolMethod(message, callbackContext);
-            return true;
+//        if (action.equals("coolMethod")) {
+//            String message = args.getString(0);
+//            this.coolMethod(message, callbackContext);
+//            return true;
+//        }
+//        return false;
+
+        switch (action) {
+            case "coolMethod":
+                Log.e(TAG, "execute coolMethod");
+                this.coolMethod(args.getString(0), callbackContext);
+                this.doPrint(args, callbackContext);
+                return true;
+            case "getExtras":
+//        this.getExtras(callbackContext);
+                return true;
+            case "doPrint":
+                this.doPrint(args, callbackContext);
+                return true;
         }
         return false;
     }
 
+    // 测试方法
     private void coolMethod(String message, CallbackContext callbackContext) {
         if (message != null && message.length() > 0) {
             callbackContext.success(message);
         } else {
             callbackContext.error("Expected one non-empty string argument.");
         }
+    }
+
+    //    打印
+    private void doPrint(JSONArray message, CallbackContext callbackContext) {
+
+        PrinterMain.bindDeviceService(this.cordova.getActivity());
+
+
+        PrinterMain printerMain = new PrinterMain();
+        printerMain.init();
+//    printerMain.addText();
+        printerMain.addBarcode();
+        printerMain.startPrint();
+
+
     }
 }
